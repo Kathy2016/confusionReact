cebc962b9041bf1fd6859a30ecdcfde4c2d6d066
Kathy <labradorleur@gmail.com>
2019-06-22 17:20:57 +0800

    Presentational and Container Components

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -27,7 +27,7 @@ class DishDetail extends Component {
                 return (
                     <li key={comment.id}>
                         <p>{comment.comment}</p>
-                        <p>--{comment.author}, {comment.date}</p>
+                        <p>--{comment.author}, {new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
                     </li>
                 );
             });
@@ -52,11 +52,12 @@ class DishDetail extends Component {
 
         if(this.props.dish != null){ 
             return(
-                <div className="row">
-                    {this.renderDish(this.props.dish)}
-                    {this.renderComments(this.props.dish.comments)}
-                </div>
-                
+                <div className="container">
+                    <div className="row">
+                        {this.renderDish(this.props.dish)}
+                        {this.renderComments(this.props.dish.comments)}
+                    </div>
+                </div>         
             );
         }
         else{
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,24 +1,14 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
-import DishDetail from './DishdetailComponent';
+
 
 class Menu extends Component{
 
     constructor(props){
         super(props);  //required whenever you define a class component
         
-        // state store in properties related to this component
-        // that we can make use of
-        this.state = {  
-            selectedDish: null      
-        }
     }
 
-    onDishSelect(dish){
-        this.setState({
-            selectedDish: dish
-        });
-    }
 
     //return corresponding view for this component
     render(){
@@ -27,7 +17,7 @@ class Menu extends Component{
             //whenever you construct a list of items in React, every item requires a key attribute to be specified for it
             return (
                 <div className="col-12 col-md-5 m-1">
-                    <Card key={dish.id} onClick={() => this.onDishSelect(dish)}> 
+                    <Card key={dish.id} onClick={() => this.props.onClick(dish.id)}> 
                         <CardImg width="100%" src={dish.image} alt={dish.name} />
                         <CardImgOverlay>
                             <CardTitle>{dish.name}</CardTitle>
@@ -42,7 +32,6 @@ class Menu extends Component{
                 <div className="row"> 
                     {menu} 
                 </div>
-                <DishDetail dish={ this.state.selectedDish } />
             </div>
         );
     }
